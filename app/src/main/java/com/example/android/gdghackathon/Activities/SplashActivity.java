81fc3e9cff874e67c673c4714120fee2c36e085b
sarthakmathur <sarthakmathur97@gmail.com>
2017-11-12 15:00:14 +0530

    minor changes

--- app/src/main/java/com/example/android/gdghackathon/Activities/SplashActivity.java
+++ app/src/main/java/com/example/android/gdghackathon/Activities/SplashActivity.java
@@ -1,16 +1,15 @@
 package com.example.android.gdghackathon.Activities;
 
 import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.example.android.gdghackathon.R;
